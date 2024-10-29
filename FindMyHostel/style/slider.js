dbf08a92a5a3bd0b35072c61780a70636b7ee045
HAMID-JUMA <worthygreatie@gmail.com>
2024-10-29 21:27:33 +0200

    Delete FindMyHostel directory

--- FindMyHostel/style/slider.js
+++
@@ -1,7 +0,0 @@
-const slider = document.getElementById("mySlider");
-const sliderValue = document.getElementById("sliderValue");
-
-// Update the displayed value as the slider moves
-slider.addEventListener("input", () => {
-    sliderValue.textContent = slider.value;
-});
